9eb31a05124b26943782ee96f161dc61a100aa9f
alexey-baldin <dumplinken@yandex.ru>
2022-08-02 12:02:44 +0300

    ProgrammersDay finished

---
+++ src/Praktikum.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Praktikum {
+    public Praktikum() {
+    }
+
+    public static void main(String[] var0) {
+        Scanner var1 = new Scanner(System.in);
+        System.out.println("Введите год:");
+        int var2 = var1.nextInt();
+        if (isLeapYear(var2)) {
+            System.out.println("12.09." + var2);
+        } else {
+            System.out.println("13.09." + var2);
+        }
+
+    }
+
+    public static boolean isLeapYear(int var0) {
+        if (var0 % 4 == 0) {
+            if (var0 % 100 == 0) {
+                return var0 % 400 == 0;
+            } else {
+                return true;
+            }
+        } else {
+            return false;
+        }
+    }
+}
